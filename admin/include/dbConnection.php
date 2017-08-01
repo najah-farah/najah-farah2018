0c2adfa4b2072f65779e846eea2b507431dfad66
Najah AL-shahrani <anaaa7077@gmail.com>
2017-08-01 20:43:30 +0100

    azure database details added

--- admin/include/dbConnection.php
+++ admin/include/dbConnection.php
@@ -2,5 +2,5 @@
 	ob_start();
 	session_start();
 	date_default_timezone_set("Asia/Kolkata");
-	$connection = mysqli_connect("localhost", "root", "", "xpertmon_photoshare") or die("Error In Connection.");
+	$connection = mysqli_connect("eu-cdbr-azure-west-b.cloudapp.net", "b9db452a40f590", "f87895d8", "xpertmon_photoshare") or die("Error In Connection.");
 ?>
