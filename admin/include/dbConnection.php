01173b63df85d46d251aff1bc32ed571745e308b
Najah AL-shahrani <anaaa7077@gmail.com>
2017-08-01 20:45:23 +0100

    Database name added.

--- admin/include/dbConnection.php
+++ admin/include/dbConnection.php
@@ -2,5 +2,5 @@
 	ob_start();
 	session_start();
 	date_default_timezone_set("Asia/Kolkata");
-	$connection = mysqli_connect("eu-cdbr-azure-west-b.cloudapp.net", "b9db452a40f590", "f87895d8", "xpertmon_photoshare") or die("Error In Connection.");
+	$connection = mysqli_connect("eu-cdbr-azure-west-b.cloudapp.net", "b9db452a40f590", "f87895d8", "acsm_439bc1316ce35ab") or die("Error In Connection.");
 ?>
